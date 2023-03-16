5e216211ea8a02fa7b929c3b0d30c3c448d6d683
felipe-de-andrade <felipe.andrade_@hotmail.com>
2023-03-15 23:59:05 -0300

    Finalizando Chekpoint

--- Chekpoint1/js/scripts.js
+++ Chekpoint1/js/scripts.js
@@ -1,94 +1,109 @@
- let board = document.getElementById('board');
- let buttonAdd = document.getElementById('add');
- let inputNm = document.getElementById('NomePersonagem');
- let UrlImg = document.getElementById('')
+let board = document.getElementById("board");
+let buttonAdd = document.getElementById("add");
+let inputNm = document.getElementById("NomePersonagem");
+let UrlImg = document.getElementById("UrlImg");
+let inputDisc = document.getElementById("DiscrPersonagem");
 
- let listaTarefas = []
-if(localStorage.getItem('listaTarefas')){
-    listaTarefas = JSON.parse(localStorage.getItem('listaTarefas'))
-}else {
-   
- localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas));
+//Verifica se já existe uma tarefa
+let listaTarefas = [];
+if (localStorage.getItem("listaTarefas")) {
+  listaTarefas = JSON.parse(localStorage.getItem("listaTarefas"));
+} else {
+  localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas));
 }
 
- mostrarNaTela(listaTarefas);
+mostrarNaTela(listaTarefas);
 
-
- inputNm.onkeypress = function(event){
-    if (event.key == "Enter") {
-        let valorDigitado = inputNm.value;
-    listaTarefas.push(valorDigitado)
-
-    gerarTarefa(valorDigitado, listaTarefas.length - 1)
-
-    localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas));
-
-    inputNm.value = ""
-
-    }
-}
-
- buttonAdd.onclick = function(){
-
+// Printa e armazena a tarefa
+inputNm.onkeypress = function (event) {
+  if (event.key == "Enter") {
     let valorDigitado = inputNm.value;
-    listaTarefas.push(valorDigitado)
+    listaTarefas.push(valorDigitado);
 
-    gerarTarefa(valorDigitado, listaTarefas.length - 1)
+    gerarTarefa(valorDigitado, listaTarefas.length - 1);
 
     localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas));
 
-    inputNm.value = ""
+    inputNm.value = "";
+    UrlImg.value = "";
+    inputDisc.value = "";
+  }
+};
 
- }
+// O Ato de apertar o botão gera
+buttonAdd.onclick = function () {
+  objeto = {
+    nome: inputNm.value,
+    imagem: UrlImg.value,
+    descricao: inputDisc.value,
+  };
+  if (
+    objeto.nome.length < 1 ||
+    objeto.imagem.length < 1 ||
+    objeto.descricao.length < 1
+  ) {
+    return alert("Preencha todos campos");
+  }
 
- function mostrarNaTela(listaTarefas){
-        // for(let item of listaTarefas){
-        //     gerarTarefa(item);
-        // }
-        board.innerHTML = ""
-        listaTarefas.forEach(function(valor, posicao){
-            gerarTarefa(valor, posicao)
-        })
- }
+  listaTarefas.push(objeto);
+  gerarTarefa(objeto, listaTarefas.length - 1);
+  localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas));
+  inputNm.value = "";
+  UrlImg.value = "";
+  inputDisc.value = "";
+};
+//Função JavaScript para renderizar uma lista de tarefas
+function mostrarNaTela(listaTarefas) {
+  board.innerHTML = "";
+  listaTarefas.forEach(function (valor, posicao) {
+    gerarTarefa(valor, posicao);
+  });
+}
 
- function gerarTarefa(valorDigitado, posicao){
-    let tarefa = document.createElement('div');
+//Função que Cria os Cards
+function gerarTarefa(valorDigitado, posicao) {
+  let tarefa = document.createElement("div");
 
-    tarefa.setAttribute('class','tarefa');
-    tarefa.setAttribute('posicao',posicao);
+  tarefa.setAttribute("class", "tarefa");
+  tarefa.setAttribute("posicao", posicao);
 
-    let titulo = document.createElement('div');
-    titulo.setAttribute('class', 'col-md-8');
-    titulo.textContent = valorDigitado;
+  let coluna = document.createElement("div");
+  coluna.setAttribute("class", "col-md-8");
 
-    let buttonCheck = document.createElement('div')
-    buttonCheck.setAttribute('class', 'col-md-2');
+  let titulo = document.createElement("h1");
+  titulo.textContent = valorDigitado.nome;
 
-    let imgCheck = document.createElement('img');
-    imgCheck.setAttribute('class','icon');
-    imgCheck.setAttribute('src','img/erro.png');
+  let img = document.createElement("img");
+  img.setAttribute("src", valorDigitado.imagem);
+  img.setAttribute('class','tamanho')
 
-    buttonCheck.appendChild(imgCheck);
+  let desc = document.createElement("p");
+  desc.textContent = valorDigitado.descricao;
 
-    imgCheck.onclick = function(event){
+  let buttonCheck = document.createElement("div");
+  buttonCheck.setAttribute("class", "col-md-2");
 
-        // let tarefaPai = event.target.parentNode.parentNode
-        // tarefaPai.remove();
-       
+  let imgCheck = document.createElement("img");
+  imgCheck.setAttribute("class", "icon");
+  imgCheck.setAttribute("src", "img/erro.png");
 
-        let posicaoTarefa = tarefa.getAttribute('posicao');
-        listaTarefas = listaTarefas.filter(function(valor, posicao){
-            return posicao != posicaoTarefa;
-        })
+  buttonCheck.appendChild(imgCheck);
 
-        mostrarNaTela(listaTarefas);
-        localStorage.setItem('listaTarefas', JSON.stringify(listaTarefas));
-        tarefa.remove();
-        
-    }
+  imgCheck.onclick = function (event) {
+    let posicaoTarefa = tarefa.getAttribute("posicao");
+    listaTarefas = listaTarefas.filter(function (valor, posicao) {
+      return posicao != posicaoTarefa;
+    });
 
-    tarefa.appendChild(titulo);
-    tarefa.appendChild(buttonCheck);
-    board.appendChild(tarefa)
- }
+    mostrarNaTela(listaTarefas);
+    localStorage.setItem("listaTarefas", JSON.stringify(listaTarefas));
+    tarefa.remove();
+  };
 
+  coluna.appendChild(titulo);
+  coluna.appendChild(img);
+  coluna.appendChild(desc);
+  tarefa.appendChild(coluna);
+  tarefa.appendChild(buttonCheck);
+  board.appendChild(tarefa);
+}
