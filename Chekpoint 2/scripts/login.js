aa057d6fbafa5cd353c1b79173d529e823b8d71d
felipe-de-andrade <felipe.andrade_@hotmail.com>
2023-04-03 20:26:19 -0300

    Voltando um Backup

--- Chekpoint 2/scripts/login.js
+++ Chekpoint 2/scripts/login.js
@@ -3,10 +3,51 @@ const inputSenha = document.getElementById("input-password");
 const errorSenha = document.getElementById("senha");
 const inputEmail = document.getElementById("input-email");
 const errorMessage = document.getElementById("error-message");
+const inputPassword = document.getElementById("input-password");
 const emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
-const passwordRegex = /^(?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[a-zA-Z]).{8,}$/;
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/;
 
 
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let  loginUsuario = {
+        email: inputEmail.value,
+        password: inputPassword.value
+      };
+
+
+      const loginUsuarioJson = JSON.stringify(loginUsuario);
+
+      let configuracoesRequisicao = {
+          method: 'POST',
+          body: loginUsuarioJson,
+          headers: {
+              'Content-type': 'application/json',
+          },
+      }
+
+      inputEmail.value = "";
+      inputPassword.value = "";
+
+
+      async function fazerLogin() {
+          try{
+              const resposta = await fetch(`https://todo-api.ctd.academy/v1/users/login`, configuracoesRequisicao);
+              let chaveJwt = await resposta.json();
+              console.log(chaveJwt.jwt);
+              window.location.href = "tarefas.html";
+          }
+          catch(erro) {
+              console.log(erro);
+          }
+      }
+      
+
+      fazerLogin();
+
+}) 
 
 inputEmail.addEventListener("blur", () => {
   if (inputEmail.value === "" || !emailRegex.test(inputEmail.value)) {
